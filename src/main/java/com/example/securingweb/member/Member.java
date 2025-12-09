dc2c8f159a8bbf6d93a00ba621ba9d1044061317
wonehgus <wonehgus1234@naver.com>
2025-12-09 16:54:33 +0900

    Initial commit

---
+++ src/main/java/com/example/securingweb/member/Member.java
@@ -0,0 +1,46 @@
+package com.example.securingweb.member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 회원정보 클래스
+ * <p>
+ * DAO 인터페이스 : {@link MemberRepository}
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //사용자 식별 번호
+
+    @Column(length = 100 , nullable = false)
+    private String email;    //이메일
+
+    @Column(length = 30 , nullable = false)
+    private String loginId;  //로그인 ID
+
+    @Column(length = 20 , nullable = false)
+    private String name;     //사용자 이름
+
+    @Column(length = 40 , nullable = false)
+    private String password; //비밀번호
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", loginId='" + loginId + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/securingweb/member/MemberRepository.java
@@ -0,0 +1,20 @@
+package com.example.securingweb.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * 회원정보({@link Member}) DAO 인터페이스
+ */
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByLoginId(String id);
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByUsername(String username);
+    
+    // 중복 확인 메서드
+    boolean existsByEmail(String email);
+    boolean existsByLoginId(String loginId);
+}
